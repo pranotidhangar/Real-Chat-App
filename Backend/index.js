c29b3575124f71195804500ece2a4512cbd37fbf
pranotidhangar <131478879+pranotidhangar@users.noreply.github.com>
2023-06-18 22:08:32 +0530

    Update index.js
    Adding Axios library

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const { default: axios } = require("axios");
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
